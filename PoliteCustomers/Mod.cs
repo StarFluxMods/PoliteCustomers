cee72583178147951d00fef9ddae455a8f329a28
Lachlan Leone <starfluxgames@gmail.com>
2022-10-25 19:15:01 +1100

    v0.1.0

---
+++ PoliteCustomers/Mod.cs
@@ -0,0 +1,37 @@
+﻿using KitchenLib;
+using KitchenLib.Event;
+using KitchenData;
+using KitchenLib.Utils;
+using HarmonyLib;
+using Kitchen;
+
+namespace PoliteCustomers
+{
+    public class Mod : BaseMod
+    {
+        public Mod() : base("politecustomers", "1.0.5") {}
+
+        public override void OnInitializeMelon()
+        {
+            Events.BuildGameDataEvent += (s, args) =>
+            {
+                foreach (Item item in args.gamedata.Get<Item>())
+                {
+                    if (item.DirtiesTo != null)
+                        if (item.DirtiesTo.ID == GDOUtils.GetExistingGDO(1517992271).ID)
+                            item.DirtiesTo = (Item)GDOUtils.GetExistingGDO(793377380);
+                }
+            };
+        }
+    }
+    
+    [HarmonyPatch(typeof(CreateNewMesses), "FindMessLocation")]
+    class CreateNewMesses_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result)
+        {
+            __result = false;
+        }
+    }
+}
---
+++ PoliteCustomers/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(PoliteCustomers.Mod), "Polite Customers", "0.1.0", "StarFluxGames")]
+[assembly: MelonGame("It's Happening", "PlateUp")]
